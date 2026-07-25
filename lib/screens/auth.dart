4e58bef72af8a329b0ab9f69df25a6588fd1c000
satoshi-sh <satoss1108@gmail.com>
2026-07-25 16:53:35 -0500

    Finished 282 Showing different screens based on the auththentication state

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -31,13 +31,11 @@ class _AuthScreenState extends State<AuthScreen> {
           email: _enteredEmail,
           password: _enteredPassword,
         );
-        print(userCredentials);
       } else {
         final userCredentials0 = _firebase.createUserWithEmailAndPassword(
           email: _enteredEmail,
           password: _enteredPassword,
         );
-        print(userCredentials0);
       }
     } on FirebaseAuthException catch (error) {
       // if (error.code == 'email-already-in-use') {}
